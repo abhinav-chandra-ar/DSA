46b5dd76743575746f7c406a4e655b567bcd29aa
chadxabhinav <abhinavcanichandran@gmail.com>
2026-06-29 10:24:10 +0530

    Calculated usind DP

---
+++ Dynamic Programming/fibinocci.py
@@ -0,0 +1,13 @@
+def fib(n,dp):
+    if n <= 1 :
+        return n
+    if dp[n] != -1:
+        return dp[n]
+    dp[n] = fib(n-1,dp) + fib(n-2,dp)
+    return dp[n]
+
+if __name__ == '__main__':
+    
+    n = int(input())
+    dp = [-1] * (n+1)
+    print(fib(n,dp))
